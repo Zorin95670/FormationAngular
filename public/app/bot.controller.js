362f13847ca9958cd054f9deeb5a667a56be9079
Vincent Moittie <moittie.vincent@gmail.com>
2017-01-10 11:59:36 +0100

    add pagination

--- public/app/bot.controller.js
+++ public/app/bot.controller.js
@@ -16,19 +16,44 @@
     $scope.sorterType = "0";
     $scope.search = "";
 
+    $scope.pagination = "5";
+    $scope.currentPagination = 1;
+
+    $scope.maxPagination = Math.ceil($scope.sentences.length / $scope.pagination);
+
     $scope.$watch('sorter', function(){
-        $scope.sentencesTable = $filter('orderBy')($scope.sentences, $scope.sorter, ($scope.sorterType == 1));
+      $scope.setPagination();
     });
     $scope.$watch('sorterType', function(){
-        $scope.sentencesTable = $filter('orderBy')($scope.sentences, $scope.sorter, ($scope.sorterType == 1));
+      $scope.setPagination();
     });
     $scope.$watch('search', function(){
-        $scope.sentencesTable = $filter('filter')($scope.sentencesTable, $scope.search);
+        $scope.setPagination();
     });
 
     $scope.deleteSentence = function(index){
       $scope.sentences.splice($scope.sentences.indexOf( $scope.sentencesTable[index]), 1);
       $scope.sentencesTable.splice(index, 1);
+    };
+
+    $scope.setMaxPagination = function(){
+      $scope.maxPagination = Math.ceil($scope.sentences.length / $scope.pagination);
+      if($scope.currentPagination > $scope.maxPagination) $scope.currentPagination = $scope.maxPagination;
+      $scope.setPagination();
+    };
+
+    $scope.setPagination = function(){
+      $scope.currentPagination = $scope.currentPagination | 1;
+      if($scope.currentPagination > $scope.maxPagination) $scope.currentPagination = $scope.maxPagination;
+      var value = $scope.currentPagination;
+
+      var index = $scope.pagination * (value-1);
+
+      $scope.sentencesTable = $filter('orderBy')($scope.sentences, $scope.sorter, ($scope.sorterType == 1))
+        .slice(index, index+$scope.pagination);
+
+      $scope.sentencesTable = $filter('filter')($scope.sentencesTable, $scope.search);
+
     };
   };
 
