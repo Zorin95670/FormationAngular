8110487aaf25451626afb1424fec6b620d7e7709
Vincent Moittie <moittie.vincent@gmail.com>
2017-01-10 10:12:20 +0100

    Second method to filter, with watcher

--- public/app/bot.controller.js
+++ public/app/bot.controller.js
@@ -2,7 +2,7 @@
   angular.module('app')
   .controller('BotController', botController);
 
-  function botController($scope){
+  function botController($scope, $filter){
     $scope.msg = "";
     $scope.sentences = [
       { word: 'a', response: '1'},
@@ -10,8 +10,21 @@
       { word: 'c', response: '3'},
       { word: 'd', response: '4'},
     ];
+    $scope.sentencesTable = $scope.sentences;
+
     $scope.sorter = "word";
-    $scope.sorterType = '0';
+    $scope.sorterType = "0";
+    $scope.search = "";
+
+    $scope.$watch('sorter', function(){
+        $scope.sentencesTable = $filter('orderBy')($scope.sentences, $scope.sorter, ($scope.sorterType == 1));
+    });
+    $scope.$watch('sorterType', function(){
+        $scope.sentencesTable = $filter('orderBy')($scope.sentences, $scope.sorter, ($scope.sorterType == 1));
+    });
+    $scope.$watch('search', function(){
+        $scope.sentencesTable = $filter('filter')($scope.sentencesTable, $scope.search);
+    });
   };
 
 })();
